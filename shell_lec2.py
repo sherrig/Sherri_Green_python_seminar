94246f80f9f54ba78da73111d0d0889bca9dd863
Sherri <sagreen@mit.edu>
2015-02-21 21:53:28 -0500

    Commit of lecture 2 materials

---
+++ shell_lec2.py
@@ -0,0 +1,68 @@
+Python 2.7.9 (v2.7.9:648dcafa7e5f, Dec 10 2014, 10:10:46) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> 1
+1
+>>> 1.3
+1.3
+>>> 1.
+1.0
+>>> 'This is a string'
+'This is a string'
+>>> len('This is a string')
+16
+>>> 93849301232890874859018234792019283748920198237483210239847
+93849301232890874859018234792019283748920198237483210239847L
+>>> True
+True
+>>> Flase
+
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    Flase
+NameError: name 'Flase' is not defined
+>>> False
+False
+>>> None
+>>> var = True
+>>> type (var)
+<type 'bool'>
+>>> var
+True
+>>> (1, 2, 3)
+(1, 2, 3)
+>>> [2, 5, 1]
+[2, 5, 1]
+>>> (1, 3.4, 'text')
+(1, 3.4, 'text')
+>>> var = (1, 3.4, 'text')
+>>> type (var)
+<type 'tuple'>
+>>> var
+(1, 3.4, 'text')
+>>> var[1]
+3.4
+>>> var [0]
+1
+>>> var[2]
+'text'
+>>> l = [2, 'string', 1.0]
+>>> l[2]
+1.0
+>>> 1/3
+0
+>>> 1.0/3.0
+0.3333333333333333
+>>> 4/3
+1
+>>> 6*3
+18
+>>> 6*3.0
+18.0
+>>> l = [2, 'string', [2,4,(0,0)]]
+>>> l
+[2, 'string', [2, 4, (0, 0)]]
+>>> 
